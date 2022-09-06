cc5472f8562ea8aa54b62b1f039f14e144ef474e
Koen Tiepel <koentiepel@live.nl>
2022-09-06 14:21:03 +0200

    03-A: Opdracht 1.3: PSD ontwerp verwerking van een formulierdata #4

--- contact.php
+++ contact.php
@@ -6,6 +6,43 @@
     </head>
 
     <body>
+       <?php
+
+         $sex = $name = $email = $phone = $preferred = $question = '';
+         $sexErr = $nameErr = $emailErr = $phoneErr = $preferredErr = $questionErr = '';
+         $valid = false;
+
+         if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            if (!isset($_POST['sex'])) {
+                $aanhefErr = "Aanhef is verplicht";
+            } else {
+                $aanhef = test_input($_POST['aanhef']);
+                
+                switch ($sex) {
+                    case 'sir':
+                    case 'madam':
+                    case 'other':
+                       break;
+
+                    default:
+                      $sexErr = "Aanhef is niet correct";
+                      break;       
+                }
+            }
+
+            if (!isset($_POST['name'])) {
+                $nameErr = "Naam is verplicht";
+            } else {
+                $name = test_input($_POST['name']);
+                if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+                    $nameErr = "Alleen letters en spaties zijn toegestaan.";
+                    }
+            }
+
+         }
+
+       ?>
         <header>Contact
         </header>
 
@@ -13,24 +50,24 @@
             <ul class="nav-tabs">
                 <li><a href="index.html">Home</a></li>
                 <li><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="contact.php">Contact</a></li>
             </ul>
         </div> 
 
         <h3>Contactformulier</h3>
 
-        <form action="/action_page.php" method="get">
+        <form action="contact.php" method="post">
             <fieldset>
-            <label for="aanhef">Aanhef:</label>
-            <select class="aanhef" aanhef="aanhef" id="aanhef" required>
+            <label for="sex">Aanhef:</label>
+            <select class="sex" name="sex" id="sex" required>
               <option value="" selected="selected">Kies aanhef</option>
               <option value="sir">De heer</option>
-              <option value="madame">Mevrouw</option>
+              <option value="madam">Mevrouw</option>
               <option value="other">Anders</option>
             </select>
             <br>
-            <label for="naam">Naam: </label>
-            <input class="naam" type="text" id="name" name="name" placeholder="Henk de Vries" maxlength="50" required>
+            <label for="name">Naam: </label>
+            <input class="name" type="text" id="name" name="name" placeholder="Henk de Vries" maxlength="50" value="<?php echo $name; ?>" required>
             <br>
             <label for="email">E-mail: </label>
             <input class="email" type="email" id="email" name="email" placeholder="henk74@gmail.com" maxlength="60" required>
@@ -41,7 +78,7 @@
                 
             <fieldset>
                 <label for="preferred">Voorkeur contact: </label>
-                <input type="radio" id="pref-1" name="preferred" value="email">
+                <input type="radio" id="pref-1" name="preferred" value="email" required>
                 <label for="pref-1" class="option">Mailen</label>
                 <input type="radio" id="pref-2" name="preferred" value="phone">
                 <label for="pref-2" class="option">Bellen</label>
